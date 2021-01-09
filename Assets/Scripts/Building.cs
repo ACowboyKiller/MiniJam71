838a92b21a649a74b71b09039ec5741b61d81433
Chris Walka <walka.christopher@gmail.com>
2021-01-09 11:40:04 -0600

    Implemented Models & Rhythm Tracking

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using RhythmTool;
+
+public class Building : MonoBehaviour
+{
+
+    #region --------------------    Public Enumerations
+
+    public enum BuildingType { Skyscraper, Shop, Apartment, Park, Restaurant, }
+
+    #endregion
+
+    #region --------------------    Public Properties
+
+
+
+    #endregion
+
+    #region --------------------    Private Fields
+
+
+
+    [SerializeField] private MeshRenderer _renderer = null;
+    private bool _isResetingBeat = false;
+
+    #endregion
+
+    #region --------------------    Private Methods
+
+    private void Start()
+    {
+        MusicManager.OnBeatEvent += _OnBeat;
+    }
+
+    private void _OnBeat(Beat _pBeat)
+    {
+        _PerformBeatVisual(_pBeat);
+    }
+
+    /// <summary>
+    /// Performs the visual portion of the beat actions
+    /// </summary>
+    /// <param name="_pBeat"></param>
+    private void _PerformBeatVisual(Beat _pBeat)
+    {
+        if (_isResetingBeat) return;
+        _isResetingBeat = true;
+        float _speed = (1f / Mathf.Max((_pBeat.bpm / 60f), 0.01f));
+        float _resetTime = _speed * 0.9f;
+        DOTween.To(() => _renderer.material.GetFloat("Vector1_Beat"), x => _renderer.material.SetFloat("Vector1_Beat", x), 1f, _speed)
+            .OnComplete(() => {
+                DOTween.To(() => _renderer.material.GetFloat("Vector1_Beat"), x => _renderer.material.SetFloat("Vector1_Beat", x), 0f, _resetTime)
+                    .OnComplete(() => { _isResetingBeat = false; });
+            });
+    }
+
+    #endregion
+
+}
--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RhythmTool;
+using DG.Tweening;
 
 public class MusicManager : MonoBehaviour
 {
@@ -9,19 +10,38 @@ public class MusicManager : MonoBehaviour
     #region --------------------    Public Properties
 
     /// <summary>
+    /// The singleton instance of the class
+    /// </summary>
+    public static MusicManager instance { get; private set; } = null;
+
+    /// <summary>
+    /// Definition for the music event
+    /// </summary>
+    public delegate void MusicEvent(Beat _pBeat);
+
+    /// <summary>
+    /// The event used to signal that a beat has occured
+    /// </summary>
+    public static MusicEvent OnBeatEvent { get; set; } = null;
+
+    /// <summary>
     /// Returns the clip for the manager.
     /// </summary>
-    public AudioClip clip => _clip;
+    public AudioClip clip => _nextClip;
 
     #endregion
 
     #region --------------------    Public Methods
 
     /// <summary>
-    /// Sets the clip with an Audio Source.
+    /// Imports the clip
     /// </summary>
-    /// <param name="_pSource"></param>
-    public void SetClip(AudioSource _pSource) => SetClip(_pSource.clip);
+    /// <param name="_pPath"></param>
+    public void ImportClip(string _pPath)
+    {
+        _importer.Loaded += SetClip;
+        _importer.Import(_pPath);
+    }
 
     /// <summary>
     /// Sets the clip for the manager.
@@ -29,24 +49,85 @@ public class MusicManager : MonoBehaviour
     /// <param name="_pClip"></param>
     public void SetClip(AudioClip _pClip)
     {
-        _clip = _pClip;
-        _data = _analyzer.Analyze(_clip);
-        Track<Beat> _track = _data.GetTrack<Beat>();
+        _importer.Loaded -= SetClip;
+        _nextClip = _pClip;
+        _analyzer.Analyze(_nextClip);
+        _isLoading = true;
     }
 
     #endregion
 
     #region --------------------    Private Fields
 
+    [SerializeField] private AudioSource _source = null;
+    [SerializeField] private Browser _browser = null;
+    [SerializeField] private AudioImporter _importer = null;
     [SerializeField] private RhythmAnalyzer _analyzer = null;
-    [SerializeField] private AudioClip _clip = null;
-    [SerializeField] private RhythmData _data = null;
+    [SerializeField] private RhythmPlayer _player = null;
+
+    private bool _isLoading = false;
+    private AudioClip _nextClip = null;
 
     #endregion
 
     #region --------------------    Private Methods
 
+    /// <summary>
+    /// Sets the singleton
+    /// </summary>
+    private void Awake() => _SetSingleton();
 
+    /// <summary>
+    /// Configures the singleton instance of the class
+    /// </summary>
+    private bool _SetSingleton()
+    {
+        instance = instance ?? this;
+        if (instance == this) return true;
+        Destroy(gameObject);
+        return false;
+    }
+
+    /// <summary>
+    /// Hookup with the file browser
+    /// </summary>
+    private void Start()
+    {
+        _browser.FileSelected += ImportClip;
+        ((RhythmEventProvider)_player.targets[0]).Register<Beat>(_OnBeat);
+    }
+
+    /// <summary>
+    /// Unregister with the event provider
+    /// </summary>
+    private void OnDestroy() => ((RhythmEventProvider)_player.targets[0]).Unregister<Beat>(_OnBeat);
+
+    /// <summary>
+    /// Lets all subscribers know that a beat has occured
+    /// </summary>
+    /// <param name="_pBeat"></param>
+    private void _OnBeat(Beat _pBeat) => OnBeatEvent?.Invoke(_pBeat);
+
+    /// <summary>
+    /// Looks to see if new music is loading in and when it's ready, does a transition to the new music.
+    /// </summary>
+    private void Update()
+    {
+        if (!_isLoading) return;
+        if (_analyzer.isDone)
+        {
+            _isLoading = false;
+            _player.rhythmData = _analyzer.rhythmData;
+            //  Fades the track out and fades in the new track
+            DOTween.To(() => _source.volume, x => _source.volume = x, 0f, 1f)
+               .OnComplete(() => {
+                   _isLoading = false;
+                   _source.clip = _nextClip;
+                   _source.Play();
+                   DOTween.To(() => _source.volume, y => _source.volume = y, 1f, 1f);
+               });
+        }
+    }
 
     #endregion
 

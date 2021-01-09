5bc9844a61ba9e763be71e6144180590fe82be87
Chris Walka <walka.christopher@gmail.com>
2021-01-08 22:14:05 -0600

    Established Project Baseline

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RhythmTool;
+
+public class MusicManager : MonoBehaviour
+{
+
+    #region --------------------    Public Properties
+
+    /// <summary>
+    /// Returns the clip for the manager.
+    /// </summary>
+    public AudioClip clip => _clip;
+
+    #endregion
+
+    #region --------------------    Public Methods
+
+    /// <summary>
+    /// Sets the clip with an Audio Source.
+    /// </summary>
+    /// <param name="_pSource"></param>
+    public void SetClip(AudioSource _pSource) => SetClip(_pSource.clip);
+
+    /// <summary>
+    /// Sets the clip for the manager.
+    /// </summary>
+    /// <param name="_pClip"></param>
+    public void SetClip(AudioClip _pClip)
+    {
+        _clip = _pClip;
+        _data = _analyzer.Analyze(_clip);
+        Track<Beat> _track = _data.GetTrack<Beat>();
+    }
+
+    #endregion
+
+    #region --------------------    Private Fields
+
+    [SerializeField] private RhythmAnalyzer _analyzer = null;
+    [SerializeField] private AudioClip _clip = null;
+    [SerializeField] private RhythmData _data = null;
+
+    #endregion
+
+    #region --------------------    Private Methods
+
+
+
+    #endregion
+
+}
